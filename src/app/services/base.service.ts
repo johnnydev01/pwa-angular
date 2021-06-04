c1138ba862e9a4faa2b12990acd4859558f4f38a
Johnny Ferreira Silva <joferres@everis.com>
2021-06-04 20:44:49 -0300

    create generics service

---
+++ src/app/services/base.service.ts
@@ -0,0 +1,89 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import Dexie from 'dexie';
+import { OnlineOfflineService } from './online-offline.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseService<T extends { id: string; }> {
+
+  protected db: Dexie;
+  table: Dexie.Table<T, any> = null;
+
+  private http: HttpClient;
+  private onlineOfflineService: OnlineOfflineService
+
+  constructor(protected injector: Injector,
+    protected nomeTabela: string,
+    protected urlApi: string) {
+    this.http = this.injector.get(HttpClient);
+    this.onlineOfflineService = this.injector.get(OnlineOfflineService);
+    this.ouvirStatusConexao();
+    this.iniciarIndexedDb();
+  }
+
+  private iniciarIndexedDb() {
+    this.db = new Dexie('db-tabelas');
+    this.db.version(1).stores({
+      [this.nomeTabela]: 'id'
+    });
+    this.table = this.db.table(this.nomeTabela);
+  }
+
+  private salvarAPI(tabela: T) {
+    this.http.post(this.urlApi, tabela)
+      .subscribe(() => {
+        alert('tabela foi cadastrado com sucesso'),
+          error => {
+            console.log("Erro ao cadastrar tabela");
+          }
+      });
+  }
+  private async salvarIndexedDb(tabela: T) {
+    try {
+      await this.table.add(tabela);
+      const todostabelas: T[] = await this.table.toArray();
+      console.log('tabela foi salvo no IndexedDb', todostabelas);
+
+    } catch (error) {
+      console.log('Erro ao tentar incerir tabela no IndexedDb', error);
+    }
+  }
+
+  private async enviarIndexedDbParaAPI(){
+    const todostabelas: T[] = await this.table.toArray();
+  
+    for (const tabela of todostabelas) {
+      this.salvarAPI(tabela);
+      await this.table.delete(tabela.id);
+      console.log(`tabela com id ${tabela.id} foi excluído com sucesso`);
+    }
+  }
+
+  salvar(tabela: T) {
+    if (this.onlineOfflineService.isOnline) {
+      this.salvarAPI(tabela);
+    } else {
+      this.salvarIndexedDb(tabela);
+    }
+  }
+
+  listar(): Observable<T[]> {
+    return this.http.get<T[]>(this.urlApi)
+  }
+
+  private ouvirStatusConexao() {
+    this.onlineOfflineService.statusConexao
+      .subscribe(online => {
+        if (online) {
+          this.enviarIndexedDbParaAPI();
+        } else {
+          console.log('Estou offline')
+        }
+      });
+  }
+
+
+}
--- src/app/services/seguro.service.ts
+++ src/app/services/seguro.service.ts
@@ -1,82 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import Dexie from 'dexie';
-import { Observable } from 'rxjs';
+import { Injectable, Injector } from '@angular/core';
 import { Seguro } from '../components/models/seguro';
-import { OnlineOfflineService } from './online-offline.service';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SeguroService {
+export class SeguroService extends BaseService<Seguro>{
 
-  private API_SEGUROS = 'http://localhost:9000/api/seguros';
-  private db: Dexie;
-  private table: Dexie.Table<Seguro, any> = null;
-
-  constructor(private http: HttpClient,
-              private onlineOfflineService: OnlineOfflineService) { 
-                this.ouvirStatusConexao();
-                this.iniciarIndexedDb();
-              }
-
-  private salvarAPI(seguro: Seguro){
-    this.http.post(this.API_SEGUROS, seguro)
-    .subscribe(()=>{
-      alert('Seguro foi cadastrado com sucesso'),
-      error=>{
-        console.log("Erro ao cadastrar seguro");
-      }
-    })
-  }
-  private async  salvarIndexedDb(seguro: Seguro){
-    try {
-      await this.table.add(seguro);
-      const todosSeguros: Seguro[] =  await this.table.toArray();
-      console.log('Seguro foi salvo no IndexedDb', todosSeguros);
-      
-    } catch (error) {
-      console.log('Erro ao tentar incerir seguro no IndexedDb', error);
-    }
+  constructor(protected injector: Injector) { 
+    super(injector, 'seguros', 'http://localhost:9000/api/seguros');
   }
 
-  private async enviarIndexedDbParaAPI(){
-    const todosSeguros: Seguro[] =  await this.table.toArray();
-    for (const seguro of todosSeguros){
-      this.salvarAPI(seguro);
-      await this.table.delete(seguro.id);
-      console.log(`Seguro com id ${seguro.id} foi excluído com sucesso`);
-    }
-  }
-
-  salvar(seguro: Seguro){
-    if(this.onlineOfflineService.isOnline){
-      this.salvarAPI(seguro);
-    } else {
-      this.salvarIndexedDb(seguro);
-    }
-  }
-
-  listar(): Observable<Seguro[]>{
-    return this.http.get<Seguro[]>(this.API_SEGUROS)
-  }
-
-  private ouvirStatusConexao(){
-    this.onlineOfflineService.statusConexao
-      .subscribe(online => {
-        if(online){
-          this.enviarIndexedDbParaAPI();
-        } else{
-          console.log('Estou offline')
-        }
-      });
-  }
-
-  private iniciarIndexedDb(){
-    this.db = new Dexie('db-seguros');
-    this.db.version(1).stores({
-      seguro: 'id'
-    });
-    this.table = this.db.table('seguro');
-  }
+ 
 }
